97c7b9fe22f8736b07f05127a621c967e30045eb
Pedro Carvalho <pmdc@ua.pt>
2021-10-27 15:56:42 +0100

    Ex 4 completo Aula3

---
+++ Aula3/Ex4/main.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+class Complex:
+
+    def __init__(self, r, i):
+        self.r = r  # store real part in class instance
+        self.i = i  # store imaginary part in class instance
+
+    def add(self, y):
+        self.r = self.r + y.r
+        self.i = self.i + y.i
+    def multiply(self, y):
+        self.r = self.r * y.r + self.i * y.i
+        self.i = self.r * y.i + self.i * y.r
+    def __str__(self):
+        return '{self.r} + {self.i}i'.format(self=self)
+
+def main():
+    # declare two instances of class two complex numbers as tuples of size two
+    c1 = Complex(5, 3)  # use order when not naming
+    c2 = Complex(i=7, r=-2)  # if items are names order is not relevant
+
+    # Test add
+    print(c1)  # uses the __str__ method in the class
+    c1.add(c2)
+    print(c1)  # uses the __str__ method in the class
+
+    # test multiply
+    print(c2)  # uses the __str__ method in the class
+    c2.add(c1)
+    print(c2)  # uses the __str__ method in the class
+
+if __name__ == '__main__':
+    main()
