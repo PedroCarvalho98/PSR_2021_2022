4781726735b534758cd2662632aefa3b6bae29bf
Pedro Carvalho <pmdc@ua.pt>
2021-11-10 15:12:53 +0000

    Aula 5 ex3a e ex3b completo

--- Aula5/Ex3/ex3b.py
+++ Aula5/Ex3/ex3b.py
@@ -6,30 +6,31 @@
 import argparse
 import cv2
 import numpy as np
+from functools import partial
 
 # Global variables
-window_name = 'window - Ex3a'
-image_gray = None
 
 
-def onTrackbar(threshold):
+def onTrackbar(threshold, image_src, window):
     # Add code here to threshold image_gray and show image in window
-    _, image_thresholded = cv2.threshold(image_gray, threshold, 255, cv2.THRESH_BINARY)
-    cv2.imshow(window_name, image_thresholded)
+    _, image_thresholded = cv2.threshold(image_src, threshold, 255, cv2.THRESH_BINARY)
+    cv2.imshow(window, image_thresholded)
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--image', type=str, required=True,help='Full path to image file.')
     args = vars(parser.parse_args())
 
+    window_name = 'window - Ex3a'
+    image_gray = None
+
     image = cv2.imread(args['image'], cv2.IMREAD_COLOR)  # Load an image
-    global image_gray # use global var
     image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)  # convert bgr to gray image (single channel)
     cv2.namedWindow(window_name)
 
     # add code to create the trackbar ...
 
-    cv2.createTrackbar("Trackbar Binary", window_name, 1, 255, onTrackbar)
+    cv2.createTrackbar("Trackbar Binary", window_name, 1, 255, partial(onTrackbar, image_src = image_gray, window = window_name))
     cv2.imshow('Original', image)  # Display the image
     cv2.waitKey(0)
 
